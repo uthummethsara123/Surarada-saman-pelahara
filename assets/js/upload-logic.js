5891bf370eda5b3ee8d3749f041bc6fd71b4e400
uthummethsara123 <uthummethsara123@gmail.com>
2026-06-17 12:10:44 +0530

    Update upload-logic.js

--- assets/js/upload-logic.js
+++ assets/js/upload-logic.js
@@ -213,8 +213,8 @@ const payload = {
 // ==========================================================
 // MARATHON TIMELINE ENGINE
 // ==========================================================
-const START_DATE = new Date("2026-06-17T10:49:00").getTime();
-const DEADLINE_DATE = new Date("2026-06-17T12:30:00").getTime();
+const START_DATE = new Date("2026-07-26T24:49:00").getTime();
+const DEADLINE_DATE = new Date("2026-07-27T02:00:00").getTime();
 
 const runTimelineEngine = () => {
     const now = new Date().getTime();
